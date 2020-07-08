64a6d9f72e9e441f85eea275fb57a7015a06a405
tamilarasumk731 <tamilarasu.mk731@gmail.com>
2020-07-09 01:11:54 +0530

    Email validation added with getmainstreet.com

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,6 +3,20 @@ class Company < ApplicationRecord
 
 
   validate :set_city_state
+  validate :check_email
+
+  def check_email
+  	return if email.blank?
+
+    if email =~ (/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i)
+    	domain = email.split('@').last
+        if domain != "getmainstreet.com"
+          errors.add(:email, "The domain #{domain} does not acceptable. Please enter a valid email address.")
+        end
+    else
+      errors.add(:email, 'Invalid email address.')
+    end
+  end
 
 
   def set_city_state
