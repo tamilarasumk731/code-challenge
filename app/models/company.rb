37da29588c648567731abf700f529cd72a17bc71
tamilarasumk731 <tamilarasu.mk731@gmail.com>
2020-07-09 00:18:48 +0530

    Zipcode based city, state updated

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,19 @@
 class Company < ApplicationRecord
   has_rich_text :description
-  
+
+
+  validate :set_city_state
+
+
+  def set_city_state
+    place_details = ZipCodes.identify(zip_code)
+    if !place_details.nil?
+	  self.city = place_details[:city]
+	  self.state = place_details[:state_code]
+	  return
+	else
+		errors.add(:zipcode, 'Invalid!')
+	end
+  end
 
 end
